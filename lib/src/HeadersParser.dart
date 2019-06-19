837a627b31ad85e22bffca060b46487bedb4f4de
Luciano Bonifacio Rodrigues <lamkrodrigues@gmail.com>
2019-06-19 08:18:33 -0300

    Moved headers parse to api package.

--- lib/src/HeadersParser.dart
+++ lib/src/HeadersParser.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutuate_api/flutuate_api.dart';
 
-@deprecated
 mixin HeadersParser
 {
   /// Parses a [Header] list to a [Map]<String,String> and returns it.
@@ -11,8 +10,10 @@ mixin HeadersParser
     Map<String,String> buildedHeaders = {};
     headers ??= [];
     for( Header header in headers ) {
-      //MapEntry<String,String> entry = parseMapEntryHeader(header);
-      MapEntry<String,String> entry = header.toMapEntry(header);
+      if( header == null ) {
+        throw ArgumentError('List of headers can not have null elements');
+      }
+      MapEntry<String,String> entry = header.toMapEntry();
       buildedHeaders.addEntries( [ entry ] );
     }
     if( contentType != null ) {
@@ -20,87 +21,5 @@ mixin HeadersParser
     }
     return buildedHeaders;
   }
-
-  /// Parses a [Header] to a [MapEntry]<String,String>.
-  ///
-  /// Elements in [Header.parameters] must not be null or an [ArgumentError]
-  /// will be throwed.
-  /// Primitives elements will be parsed with ```toString()```.
-  /// [Map] elements, will be parsed with method [parseMapHeaderParameter],
-  /// and [List] elements with [parseListHeaderParameter].
-  MapEntry<String, String> parseMapEntryHeader(Header header) {
-    if( header == null ) {
-      throw ArgumentError('Header parameter cannot be null');
-    }
-    String value = '${header.value.toString()}';
-    if( header.parameters.isNotEmpty ) {
-      value += ';';
-    }
-    int parametersCount = 0;
-    for( var parameter in header.parameters ) {
-      if( ++parametersCount > 1 ) {
-        value += ',';
-      }
-      if(parameter is Map) {
-        value += parseMapHeaderParameter(parameter);
-      }
-      else if(parameter is List) {
-        value += parseListHeaderParameter(parameter);
-      }
-      else  {
-        value += parameter.toString();
-      }
-    }
-    return new MapEntry<String,String>(header.name, value);
-  }
-
-  /// Parses a [List] header ([Header]) parameter to [String].
-  ///
-  /// [Map] elements will be parsed with [parseMapHeaderParameter] and
-  /// [List] elements with this method.
-  String parseListHeaderParameter(List listParameter) {
-    listParameter ??= [];
-    String parsedParameter = '';
-    for( var element in listParameter ) {
-      if( parsedParameter.isNotEmpty ) {
-        parsedParameter += ',';
-      }
-      if( element is Map ) {
-        parsedParameter += parseMapHeaderParameter(element);
-      }
-      else if( element is List ) {
-        parsedParameter += parseListHeaderParameter(element);
-      }
-      else {
-        parsedParameter += element.toString();
-      }
-    }
-    return parsedParameter;
-  }
-
-  /// Parses a [Map] header ([Header]) parameter to [String].
-  ///
-  /// [Map] elements will be parsed with this method and
-  /// [List] elements with [parseListHeaderParameter].
-  String parseMapHeaderParameter(Map mapParameter) {
-    String parsedParameter = '';
-    for( var entry in mapParameter.entries ) {
-      if( parsedParameter.isNotEmpty ) {
-        parsedParameter += ',';
-      }
-      parsedParameter += '${entry.key}=';
-      if( entry.value is Map ) {
-        parsedParameter += parseMapHeaderParameter(entry.value);
-      }
-      else if( entry.value is List ) {
-        parsedParameter += parseListHeaderParameter(entry.value);
-      }
-      else {
-        parsedParameter += entry.value.toString();
-      }
-    }
-    return parsedParameter;
-  }
-
 }
 
